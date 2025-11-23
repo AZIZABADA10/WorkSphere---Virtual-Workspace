a323855c216db103dfef3a750013983975919af4
AZIZABADA10 <abadaaziz288@gmail.com>
2025-11-23 17:20:22 +0000

    add: l'ajoute de taille max de chaque zone

--- main.js
+++ main.js
@@ -95,7 +95,7 @@ formAjoute.addEventListener('submit', (e) => {
         email: emailEmp,
         telephone: teleEmp,
         url: urlEmp,
-        zoneActuelle: null,
+        tailleZoneActuelle: null,
         experiences: [...experiencesTem]
     }
 
@@ -121,29 +121,29 @@ function ajouterZoneExperiences() {
     let experElementId = Date.now();
     experElement.id = `exper-${experElementId}`;
     experElement.innerHTML = `
-    <h1 class="text-white font-semibold">Experiences </h1>
-    <div  class="flex flex-col gap-2 border-1 border-gray-400">
-        <label class="font-semibold font-white">Intitulé de poste: </label>
-        <input type="text" class="titreEx rounded"> 
-        <div class="regextitre"></div>
-        <label class="font-semibold font-white">Entreprise: </label>
-        <input type="text" class="EntrepriseEx rounded">
-        <div class="regexEntrepriseEx"></div>
-        <label class="font-semibold font-white">Description:</label>
-        <textarea name=""  class="discriptionEx max-h-[200px]  rounded"></textarea>
-        <div class="regexEntrepriseDescription"></div>
-        <label class="font-semibold font-white">Date Début:</label>
-        <input type="date" class="dateDebutEx rounded">
-        <div class="regexDateDebut"></div>
-        <label class="font-semibold font-white">Date Fin:</label>
-        <input type="date" class="dateFinEx rounded">
-        <div class="regexDateFin"></div>
-        <div class="buttons flex justify-end mr-1">
-            <button type="button" onclick="confermerExperElement(${experElementId})"><i class='bx  bx-check-circle text-xl text-green-900'></i>  </button>
-            <button type="button" onclick="supprimerExperElemnt('${experElementId}')"><i class='bx  bx-trash text-xl text-red-900'></i> </button>
+        <h1 class="text-white font-semibold">Experiences </h1>
+        <div  class="flex flex-col gap-2 border-1 border-gray-400">
+            <label class="font-semibold font-white">Intitulé de poste: </label>
+            <input type="text" class="titreEx rounded"> 
+            <div class="regextitre"></div>
+            <label class="font-semibold font-white">Entreprise: </label>
+            <input type="text" class="EntrepriseEx rounded">
+            <div class="regexEntrepriseEx"></div>
+            <label class="font-semibold font-white">Description:</label>
+            <textarea name=""  class="discriptionEx max-h-[200px]  rounded"></textarea>
+            <div class="regexEntrepriseDescription"></div>
+            <label class="font-semibold font-white">Date Début:</label>
+            <input type="date" class="dateDebutEx rounded">
+            <div class="regexDateDebut"></div>
+            <label class="font-semibold font-white">Date Fin:</label>
+            <input type="date" class="dateFinEx rounded">
+            <div class="regexDateFin"></div>
+            <div class="buttons flex justify-end mr-1">
+                <button type="button" onclick="confermerExperElement(${experElementId})"><i class='bx  bx-check-circle text-xl text-green-900'></i>  </button>
+                <button type="button" onclick="supprimerExperElemnt('${experElementId}')"><i class='bx  bx-trash text-xl text-red-900'></i> </button>
+            </div>
         </div>
-    </div>
-    `;
+        `;
 
     listeExperiences.appendChild(experElement);
     ;
@@ -251,10 +251,10 @@ function affichierEmployeesSurNonAssigne() {
 
         const carteEmplo = document.createElement('div');
         carteEmplo.innerHTML = `
-            <div class="flex justify-center text-red-700 mt-56 ml-2 rounded">
-                <p class="font-semibold">Accune employees disponible pour le moment</p>
-            </div>
-        `;
+                <div class="flex justify-center text-red-700 mt-56 ml-2 rounded">
+                    <p class="font-semibold">Accune employees disponible pour le moment</p>
+                </div>
+            `;
         zoneListeEmployeesNonAssignes.appendChild(carteEmplo);
     } else {
 
@@ -262,15 +262,15 @@ function affichierEmployeesSurNonAssigne() {
             const carteEmplo = document.createElement('div');
             carteEmplo.classList.add('bg-white', 'rounded', 'p-1', 'm-1')
             carteEmplo.innerHTML = `
-                <div class="flex bg-white ">
-                    <img src="${e.url}" class="w-10 h-10 rounded-full cursor-pointer" onclick="ouvrirModalDetailsEmployee(${e.id})">
-                    <div class="ml-2">
-                    <h4 class="font-semibold">${e.nom}</h4>
-                    <p class="text-gray-500">${e.role}</p>
-                </div>
-                </div>
-                
-            `;
+                    <div class="flex bg-white ">
+                        <img src="${e.url}" class="w-10 h-10 rounded-full cursor-pointer" onclick="ouvrirModalDetailsEmployee(${e.id})">
+                        <div class="ml-2">
+                        <h4 class="font-semibold">${e.nom}</h4>
+                        <p class="text-gray-500">${e.role}</p>
+                    </div>
+                    </div>
+                    
+                `;
             zoneListeEmployeesNonAssignes.appendChild(carteEmplo);
         });
     }
@@ -322,56 +322,56 @@ function ouvrirListeEmployes(zone) {
     listeEmployeesPossible.className = 'fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50';
 
     let contenuModal = `
-        <div class="bg-white rounded-lg w-auto h-auto overflow-hidden flex flex-col">
-            <div class="flex justify-between items-center gap-4 p-4 border-b">
-                <h2 class="text-xl font-bold">Liste des Employés - ${zone}</h2>
-                <button onclick="fermerListeEmployes()" class="text-gray-500 text-2xl font-semi-bold">
-                    X
-                </button>
-            </div>
-            <div class="p-4 overflow-y-auto flex-1">
-    `;
+            <div class="bg-white rounded-lg w-auto h-auto overflow-hidden flex flex-col">
+                <div class="flex justify-between items-center gap-4 p-4 border-b">
+                    <h2 class="text-xl font-bold">Liste des Employés - ${zone}</h2>
+                    <button onclick="fermerListeEmployes()" class="text-gray-500 text-2xl font-semi-bold">
+                        X
+                    </button>
+                </div>
+                <div class="p-4 overflow-y-auto flex-1">
+        `;
 
     if (employeeAafficher.length !== 0) {
         //console.log('Employés trouvés:', employeeAafficher);
 
         employeeAafficher.forEach(em => {
             contenuModal += `        
-                <div class="flex justify-between items-center gap-4 border p-2 rounded-xl mb-2 hover:bg-gray-50 transition">
-                    <div class="flex items-center gap-3">
-                         <img src="${em.url}" class="w-10 h-10 rounded-full cursor-pointer" onclick="ouvrirModalDetailsEmployee(${em.id})">
-                        <div>
-                            <p class="font-semibold text-gray-800">${em.nom}</p>
-                            <p class="text-sm text-gray-500">${em.email}</p>
+                    <div class="flex justify-between items-center gap-4 border p-2 rounded-xl mb-2 hover:bg-gray-50 transition">
+                        <div class="flex items-center gap-3">
+                            <img src="${em.url}" class="w-10 h-10 rounded-full cursor-pointer" onclick="ouvrirModalDetailsEmployee(${em.id})">
+                            <div>
+                                <p class="font-semibold text-gray-800">${em.nom}</p>
+                                <p class="text-sm text-gray-500">${em.email}</p>
+                            </div>
+                        </div>
+                        <div class="flex items-center gap-2">
+                            <span class="bg-blue-100 text-blue-800 px-2 py-1 rounded text-sm">${em.role}</span>
+                            <button onclick="assignerEmploye(${em.id}, '${zone}')" 
+                                    class="bg-green-600 hover:bg-green-700 rounded px-2 py-1 text-white font-medium transition">
+                                Assigner
+                            </button>
                         </div>
                     </div>
-                    <div class="flex items-center gap-2">
-                        <span class="bg-blue-100 text-blue-800 px-2 py-1 rounded text-sm">${em.role}</span>
-                        <button onclick="assignerEmploye(${em.id}, '${zone}')" 
-                                class="bg-green-600 hover:bg-green-700 rounded px-2 py-1 text-white font-medium transition">
-                            Assigner
-                        </button>
-                    </div>
-                </div>
-                
-            `;
+                    
+                `;
         });
     } else {
         //console.log('Aucun employé disponible');
         contenuModal += `
-            <div class="flex justify-center items-center bg-red-50 rounded p-6">
-                <p class="text-red-600 font-semibold">Aucun employé disponible pour cette zone</p>
-            </div>
-        `;
+                <div class="flex justify-center items-center bg-red-50 rounded p-6">
+                    <p class="text-red-600 font-semibold">Aucun employé disponible pour cette zone</p>
+                </div>
+            `;
     }
 
     contenuModal += `
-                    <button onclick="fermerListeEmployes()" class="text-white px-2 py-1 rounded bg-red-600 mt-2 font-semibold">
-                    Annuler
-                </button>
+                        <button onclick="fermerListeEmployes()" class="text-white px-2 py-1 rounded bg-red-600 mt-2 font-semibold">
+                        Annuler
+                    </button>
+                </div>
             </div>
-        </div>
-    `;
+        `;
 
     listeEmployeesPossible.innerHTML = contenuModal;
     document.body.appendChild(listeEmployeesPossible);
@@ -387,7 +387,55 @@ function fermerListeEmployes() {
 function assignerEmploye(idEmplAassigner, zoneAassiger) {
     let employees = JSON.parse(localStorage.getItem('employees')) || [];
     let indexEmployeeAAssigner = employees.findIndex(e => e.id === idEmplAassigner);
-    employees[indexEmployeeAAssigner].zoneActuelle = zoneAassiger;
+
+    // Définir les limites max pour chaque zone
+    const maxParZone = {
+        zone_de_conference: 4,
+        zone_de_serveurs: 2,
+        zone_de_securite: 2,
+        zone_de_reception: 1,
+        zone_du_personnel: 3,
+        zone_d_archives: 2
+    };
+
+
+    let tailleZoneActuelle;
+    switch (zoneAassiger) {
+        case "zone_de_conference":
+            tailleZoneActuelle = zoneDeConference;
+            break;
+        case "zone_de_serveurs":
+            tailleZoneActuelle = zoneDeServeurs;
+            break;
+        case "zone_de_securite":
+            tailleZoneActuelle = zoneDeSecurite;
+            break;
+        case "zone_de_reception":
+            tailleZoneActuelle = zoneDeReception;
+            break;
+        case "zone_du_personnel":
+            tailleZoneActuelle = zoneDePersonnel;
+            break;
+        case "zone_d_archives":
+            tailleZoneActuelle = zoneDArchives;
+            break;
+    }
+
+    if (tailleZoneActuelle.length >= maxParZone[zoneAassiger]) {
+        Swal.fire({
+            title: "Impossible d’assigner",
+            text: "Cette zone est pleine ! (max 2 employés)",
+            icon: "warning",
+            allowOutsideClick: true,
+            customClass: {
+                popup: 'swal2-popup-custom'
+            }
+        });
+
+        return;
+    }
+
+    employees[indexEmployeeAAssigner].tailleZoneActuelle = zoneAassiger;
     localStorage.setItem('employees', JSON.stringify(employees));
 
     switch (zoneAassiger) {
@@ -415,13 +463,13 @@ function assignerEmploye(idEmplAassigner, zoneAassiger) {
             zoneDArchives.push(employees[indexEmployeeAAssigner]);
             localStorage.setItem('zoneDArchives', JSON.stringify(zoneDArchives))
             break;
-
     }
+
     fermerListeEmployes();
     affichierEmployeesSurNonAssigne();
     afficherToutesLesZones();
-
 }
+
 
 
 function affichierEmployeesDansSonZone(listeDeZone, IdZoneSurHtml) {
@@ -437,14 +485,14 @@ function affichierEmployeesDansSonZone(listeDeZone, IdZoneSurHtml) {
         const cartEmp = document.createElement('div');
         cartEmp.classList = "";
         cartEmp.innerHTML = `
-            <div class="bg-white grow rounded-full flex justify-between w-36 items-center mb-2 p-1">
-                    <div class="flex gap-2">
-                         <img src="${e.url}" class="w-10 h-10 rounded-full cursor-pointer" onclick="ouvrirModalDetailsEmployee(${e.id})">
-                        <p class="font-semibold">${e.nom}</p>
-                    </div>
-                    <button class="text-red-600" onclick="retirerEmploye(${e.id}, '${IdZoneSurHtml}')">X</button>
-            </div>
-        `;
+                <div class="bg-white grow rounded-full flex justify-between w-36 items-center mb-2 p-1">
+                        <div class="flex gap-2">
+                            <img src="${e.url}" class="w-10 h-10 rounded-full cursor-pointer" onclick="ouvrirModalDetailsEmployee(${e.id})">
+                            <p class="font-semibold">${e.nom}</p>
+                        </div>
+                        <button class="text-red-600" onclick="retirerEmploye(${e.id}, '${IdZoneSurHtml}')">X</button>
+                </div>
+            `;
 
         zoneEmp.appendChild(cartEmp);
     })
@@ -457,7 +505,7 @@ function retirerEmploye(idARetirer, zoneHtml) {
     let employees = JSON.parse(localStorage.getItem('employees')) || [];
     let empIndex = employees.findIndex(e => e.id === idARetirer);
 
-    employees[empIndex].zoneActuelle = null;
+    employees[empIndex].tailleZoneActuelle = null;
     localStorage.setItem('employees', JSON.stringify(employees));
 
     switch (zoneHtml) {
@@ -547,31 +595,31 @@ function ouvrirModalDetailsEmployee(employeeId) {
     modal.className = 'fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50';
 
     modal.innerHTML = `
-        <div class="bg-white rounded-lg w-96 p-4 relative">
-            <button onclick="fermerModalDetailsEmployee()" 
-                class="absolute top-2 right-2 text-red-600 text-xl font-bold">X</button>
-            <div class="flex flex-col items-center gap-2">
-                <div class="w-full" >
-                    <img src="${employeeAafficheer.url}" class="w-30  ml-24 border-2 rounded-full h-30 shadow-md object-cover">
-                    <h2 class="text-xl mt-6 font-bold">Nom: ${employeeAafficheer.nom}</h2>
-                    <p class="text-gray-600"><b>Role:</b> ${employeeAafficheer.role}</p>
-                    <p class="text-gray-600"><b>Email:</b> ${employeeAafficheer.email}</p>
-                    <p class="text-gray-600"><b>Téléphone:</b> ${employeeAafficheer.telephone}</p>
-                    <h3 class="font-semibold mt-2">Expériences:</h3>
-                </div>
+            <div class="bg-white rounded-lg w-96 p-4 relative">
+                <button onclick="fermerModalDetailsEmployee()" 
+                    class="absolute top-2 right-2 text-red-600 text-xl font-bold">X</button>
+                <div class="flex flex-col items-center gap-2">
+                    <div class="w-full" >
+                        <img src="${employeeAafficheer.url}" class="w-30  ml-24 border-2 rounded-full h-30 shadow-md object-cover">
+                        <h2 class="text-xl mt-6 font-bold">Nom: ${employeeAafficheer.nom}</h2>
+                        <p class="text-gray-600"><b>Role:</b> ${employeeAafficheer.role}</p>
+                        <p class="text-gray-600"><b>Email:</b> ${employeeAafficheer.email}</p>
+                        <p class="text-gray-600"><b>Téléphone:</b> ${employeeAafficheer.telephone}</p>
+                        <h3 class="font-semibold mt-2">Expériences:</h3>
+                    </div>
 
-                <div class="w-full max-h-48 overflow-y-auto border p-2 rounded">
-                    ${employeeAafficheer.experiences.length > 0 ? employeeAafficheer.experiences.map(exp => `
-                        <div class="mb-2">
-                            <p class="font-semibold">${exp.titre} - ${exp.entreprise}</p>
-                            <p class="text-sm text-gray-500">${exp.description}</p>
-                            <p class="text-sm text-gray-500">${exp.dateDebut} a ${exp.dateFin}</p>
-                        </div>
-                    `).join('') : `<p class="text-gray-500">Aucune expérience</p>`}
+                    <div class="w-full max-h-48 overflow-y-auto border p-2 rounded">
+                        ${employeeAafficheer.experiences.length > 0 ? employeeAafficheer.experiences.map(exp => `
+                            <div class="mb-2">
+                                <p class="font-semibold">${exp.titre} - ${exp.entreprise}</p>
+                                <p class="text-sm text-gray-500">${exp.description}</p>
+                                <p class="text-sm text-gray-500">${exp.dateDebut} a ${exp.dateFin}</p>
+                            </div>
+                        `).join('') : `<p class="text-gray-500">Aucune expérience</p>`}
+                    </div>
                 </div>
             </div>
-        </div>
-    `;
+        `;
 
     document.body.appendChild(modal);
 }
